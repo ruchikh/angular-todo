323744238d28b76299fbf9cf5162020ba693eb9c
ruchikh <ruchikharwar4@gmail.com>
2020-04-10 18:17:39 +0530

    'add-item-in-localstorage'

--- todo.js
+++ todo.js
@@ -2,15 +2,17 @@
 const myApp = angular.module("myList", []);
 
 myApp.controller("myListController", function ($scope) {
-    $scope.items = ["AngularJs", "Reactjs", "TypeScript"];
+    $scope.items = JSON.parse(localStorage.getItem("todoArray")) || ["AngularJs", "Reactjs", "TypeScript"];
     $scope.newItem = ""
 
     $scope.addItem = function () {
-        $scope.items.push($scope.newItem)
+        $scope.items.push($scope.newItem);
+        localStorage.setItem("todoArray", JSON.stringify($scope.items));
         $scope.newItem = ""
     }
 
     $scope.deleteItem = function (index) {
         $scope.items.splice(index, 1);
+        localStorage.setItem("todoArray", JSON.stringify($scope.items));
     }
 });
